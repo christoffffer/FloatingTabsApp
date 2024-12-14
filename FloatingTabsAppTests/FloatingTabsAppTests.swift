443c109ea9a8c8d0ff9e6abc72cd743310b4bc1a
Chris S Taudien <christoffer@italpassione.se>
2024-12-14 19:32:58 +0100

    Initial Commit

---
+++ FloatingTabsAppTests/FloatingTabsAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingTabsAppTests.swift
+//  FloatingTabsAppTests
+//
+//  Created by Christoffer Shokofan Taudien on 14/12/24.
+//
+
+import Testing
+@testable import FloatingTabsApp
+
+struct FloatingTabsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
